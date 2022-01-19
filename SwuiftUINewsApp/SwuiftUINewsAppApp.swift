1f85349a64f4fdb16e202574c826154c7c3896a0
Takasur Azeem <takasurazeem@gmail.com>
2022-01-19 17:33:27 +0500

    Initial Commit

---
+++ SwuiftUINewsApp/SwuiftUINewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwuiftUINewsAppApp.swift
+//  SwuiftUINewsApp
+//
+//  Created by Takasur Azeem on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwuiftUINewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
