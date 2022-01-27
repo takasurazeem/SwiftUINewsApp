60a67b45c7052fb827ff21ff58a0ceedb9b2ece7
Takasur Azeem <takasurazeem@gmail.com>
2022-01-27 17:17:12 +0500

    ErrorView incase an error occurs
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ SwuiftUINewsApp/View/ErrorView.swift
@@ -0,0 +1,59 @@
+//
+//  ErrorView.swift
+//  SwuiftUINewsApp
+//
+//  Created by Takasur Azeem on 27/01/2022.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    
+    typealias ErrorViewActionHandler = () -> Void
+    let error: Error
+    
+    internal init(error: Error,
+                  handler: @escaping ErrorView.ErrorViewActionHandler) {
+        self.error = error
+        self.handler = handler
+    }
+
+    
+    let handler: ErrorViewActionHandler
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "exclamationmark.icloud.fill")
+                .foregroundColor(.gray)
+                .font(.system(size: 50, weight: .heavy))
+                .padding(.bottom, 4)
+            Text("Ooopss")
+                .foregroundColor(.black)
+                .font(.system(size: 30, weight: .heavy))
+            Text(error.localizedDescription)
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 4)
+            Button {
+                handler()
+            } label: {
+                Text("Retry")
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal, 30)
+            .foregroundColor(.white)
+            .background(.blue)
+            .font(.system(size: 15, weight: .heavy))
+            .cornerRadius(10)
+            
+        }
+    }
+}
+
+struct ErrorView_Preview: PreviewProvider {
+    static var previews: some View {
+        ErrorView(error: APIError.decodingError) {}
+            .previewLayout(.sizeThatFits)
+    }
+}
