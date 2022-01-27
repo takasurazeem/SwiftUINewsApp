dfbf6f4b3d7c32f9f40a958d97a780c1ad82ad00
Takasur Azeem <takasurazeem@gmail.com>
2022-01-27 18:51:41 +0500

    Divide homeview into feedview and settingsview
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- SwuiftUINewsApp/View/HomeView.swift
+++ SwuiftUINewsApp/View/HomeView.swift
@@ -10,53 +10,26 @@ import UIKit
 
 struct HomeView: View {
     
-    @Environment(\.openURL) var openUrl
-    @StateObject var viewModel = NewsViewModelImpl(service: NewsServiceImpl())
-    
     init() {
         UITabBar.appearance().unselectedItemTintColor = .gray
         UITabBar.appearance().barTintColor = .lightGray
     }
     
     var body: some View {
-        NavigationView {
-            Group {
-                switch viewModel.state {
-                case .loading:
-                    ProgressView()
-                case .failed(error: let error):
-                    ErrorView(error: error, handler: viewModel.getArticles)
-                case .success(let articles):
-                    TabView {
-                        List(articles) { article in
-                            ArticleView(article: article)
-                                .onTapGesture {
-                                    load(url: article.url)
-                                }
-                        }
-                        .navigationTitle(Text("News"))
-                        .tabItem {
-                            Image(systemName: "newspaper.fill")
-                            Text("News Feed")
-                        }
-                        Text("Settings")
-                            .navigationTitle(Text("Settings"))
-                            .tabItem {
-                                Image(systemName: "gearshape.fill")
-                                Text("Settings")
-                            }
-                    }
-                    .accentColor(.black)
+        TabView {
+            FeedView()
+                .tabItem {
+                    Image(systemName: "newspaper")
+                    Text("Feed")
                 }
-            }
-        }.onAppear(perform: viewModel.getArticles)
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("Settings")
+                }
+        }
     }
     
-    func load(url: String?) {
-        guard let link = url,
-              let url = URL(string: link) else { return }
-        openUrl(url)
-    }
 }
 
 struct HomeView_Previews: PreviewProvider {
