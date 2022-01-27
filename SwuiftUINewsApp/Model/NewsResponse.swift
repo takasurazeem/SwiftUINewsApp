b12d0ed3613ca29e0602076b18c2632ebcf58bfe
Takasur Azeem <takasurazeem@gmail.com>
2022-01-27 17:09:35 +0500

    Adds dummy article
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- SwuiftUINewsApp/Model/NewsResponse.swift
+++ SwuiftUINewsApp/Model/NewsResponse.swift
@@ -36,3 +36,19 @@ extension Article: Identifiable {
         UUID()
     }
 }
+
+extension Article {
+    
+    static var dummyArticle: Article {
+            Article(
+            author: "Andrew E. Kramer",
+            url: "https://www.nytimes.com/live/2022/01/27/world/ukraine-russia-us",
+            source: "New York Times",
+            title: "Latest on Russia-Ukraine Tensions: Live Updates - The New York Times",
+            articleDescription: "There was no immediate sign of a connection between the shooting and Russia’s military buildup at the border. The incident came hours after the U.S. offered a written response to Russian demands over Eastern Europe.",
+            image: "https://static01.nyt.com/images/2022/01/27/world/27ukraine-shooting-01/27ukraine-shooting-01-facebookJumbo.jpg",
+            date: Date()
+            )
+    }
+    
+}
